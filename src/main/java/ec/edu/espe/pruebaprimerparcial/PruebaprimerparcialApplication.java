a423b466bbeb05b860aa14740d7b8e5425b9155b
Charlie <carodriguez19@espe.edu.ec>
2023-12-20 17:31:14 -0500

    initial configuration before exam

---
+++ src/main/java/ec/edu/espe/pruebaprimerparcial/PruebaprimerparcialApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaprimerparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaprimerparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaprimerparcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/pruebaprimerparcial/PruebaprimerparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.pruebaprimerparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaprimerparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
